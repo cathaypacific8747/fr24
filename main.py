5f19ae85a79f116b61d612ddb9c5bac6eaebd0b9
cathaypacific8747 <58929011+cathaypacific8747@users.noreply.github.com>
2023-11-16 00:21:38 +0800

    refactor: remove broken code

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
 import secrets
 import base64
 import struct
 from collections.abc import MutableMapping
-import rich
-import urllib3
 import httpx
 import asyncio
 from loguru import logger
@@ -36,11 +33,7 @@ DEFAULT_REQUEST = LiveFeedRequest(
     stats=True,
     limit=1500,
     maxage=14400,
-    # selected_flightid=[0x31da4a31, 0x31da4a31],
-    selected_flightid=[ 836705876, 836711386, 836709426, 836686454 ],
 )
-# print(base64.b64encode(DEFAULT_REQUEST.SerializeToString()).decode("utf-8"))
-# raise SystemExit
 DEFAULT_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0',
     'Accept': '*/*',
@@ -93,46 +86,6 @@ def flatten(d: dict, parent_key='', sep='.'):
         else:
             items.append((new_key, value))
     return dict(items)
-
-def download(device_id: str = None):
-    if not device_id:
-        device_id = f"web-{secrets.token_urlsafe(32)}"
-
-    url = 'https://data-feed.flightradar24.com/fr24.feed.api.v1.Feed/LiveFeed'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0',
-        'Accept': '*/*',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'fr24-device-id': device_id,
-        'x-envoy-retry-grpc-on': 'unavailable',
-        'Content-Type': 'application/grpc-web+proto',
-        'X-User-Agent': 'grpc-web-javascript/0.1',
-        'X-Grpc-Web': '1',
-        'Origin': 'https://www.flightradar24.com',
-        'DNT': '1',
-        'Connection': 'keep-alive',
-        'Referer': 'https://www.flightradar24.com/',
-        'Sec-Fetch-Dest': 'empty',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Site': 'same-site',
-        'TE': 'trailers'
-    }
-
-    data = b'\x00\x00\x00\x00>\n\x14\r\x1f\x85\xbfA\x15\xecQ\xaaA\x1d\x14\xae\xdbB%\xb8\x9e\xecB\x12\x1c\n\n\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x12\x0c\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\n\x0b\x18\x030\x018\xdc\x0b@\xc0pR\x00'
-
-    http = urllib3.PoolManager()
-    response = http.request('POST', url, headers=headers, body=data)
-    with open(f'log/{time.time()*1000:.0f}_raw.txt', 'wb') as f:
-        f.write(response.data)
-    
-    logger.info(f"Downloaded {len(response.data)} bytes")
-
-def scrape():
-    while True:
-        download()
-        time.sleep(5)
-
 
 def generate_post_data(west, east):
     request = LiveFeedRequest()
@@ -191,22 +144,6 @@ async def main():
         pq.write_table(tbl, "tmp/test.parquet")
         csv.write_csv(tbl, "tmp/test.csv")
 
-# def req():
-#     data = generate_post_data(100, 110)
-#     print(data)
-
-#     url = 'https://data-feed.flightradar24.com/fr24.feed.api.v1.Feed/LiveFeed'
-#     headers = DEFAULT_HEADERS.copy()
-#     headers['fr24-device-id'] = f"web-{secrets.token_urlsafe(32)}"
-
-#     http = urllib3.PoolManager()
-#     response = http.request('POST', url, headers=headers, body=data)
-#     handle_response(response.data)
-
 if __name__ == '__main__':
     # scrape()
-    # analyse(f"1693476179790_raw.txt")
-    
-    # req()
-    # run main
     asyncio.run(main())
