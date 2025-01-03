60218b2a85904a1591c9e6f326da0c88b67dbf1a
Xavier Olive <git@xoolive.org>
2025-01-03 21:44:38 +0100

    support for XDG_CONFIG_HOME and XDG_CACHE_HOME variables

--- src/fr24/__init__.py
+++ src/fr24/__init__.py
@@ -1,8 +1,14 @@
+import os
 from pathlib import Path
 
 from appdirs import user_cache_dir, user_config_dir
 
 PATH_CACHE = Path(user_cache_dir("fr24"))
+if cache_path := os.environ.get("XDG_CACHE_HOME"):
+    PATH_CACHE = Path(cache_path) / "fr24"
+
 PATH_CONFIG = Path(user_config_dir("fr24"))
+if config_path := os.environ.get("XDG_CONFIG_HOME"):
+    PATH_CONFIG = Path(config_path) / "fr24"
 
 FP_CONFIG_FILE = PATH_CONFIG / "fr24.conf"
